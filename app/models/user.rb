a79b0be8404f686e7b63ac7f372fdcb985fce501
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-13 12:57:34 +0900

    password 名前に関してバリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/ }
+  validates :last_name_kana, presence: true, format: { with: /\p{katakana}/ }
   validates :birth_date, presence: true
 
   has_many :items
