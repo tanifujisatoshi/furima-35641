5e8b43009597fe9c9644c08b4f8497458740ff41
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-13 12:06:00 +0900

    新規登録　ログイン　ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
   end
 
 end
