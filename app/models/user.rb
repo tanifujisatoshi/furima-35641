fab4fd04f1c8055cc108af3e20e30c8626d8ae84
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-14 20:40:34 +0900

    presence: trueをまとめました

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
-  validates :last_name_kana, presence: true, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
-  validates :birth_date, presence: true
-
   has_many :items
   has_many :purchases
+
+  with_options presence: true do
+   validates :nickname 
+   validates :email, uniqueness: { case_sensitive: false }
+   validates :password,  length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+   validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+   validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+   validates :first_name_kana,  format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ }
+   validates :last_name_kana,  format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ }
+   validates :birth_date
+   
+  end
 end
+
+  
+
