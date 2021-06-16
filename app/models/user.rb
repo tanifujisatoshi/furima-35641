c7c2a25c17ec3bc2a680c2a8cb4ea7c5a1e20603
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-16 14:34:43 +0900

    正規表現をまとめました！

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :purchases
+  # has_many :items
+  # has_many :purchases
 
   with_options presence: true do
    validates :nickname 
    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
-   validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
-   validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+   validates :first_name
+   validates :last_name
   end
 
   with_options format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ } do
-   validates :first_name_kana,  format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ }
-   validates :last_name_kana,  format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ }
+   validates :first_name_kana
+   validates :last_name_kana
   end
    validates :birth_date
    
