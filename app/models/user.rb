3d618e6a32c1fed743998282d3c30ab43a588e72
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-16 13:35:57 +0900

    同一の正規表現をまとめました

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,17 @@ class User < ApplicationRecord
 
   with_options presence: true do
    validates :nickname 
-   validates :email, uniqueness: { case_sensitive: false }
-   validates :password,  length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
    validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
    validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  end
+
+  with_options format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ } do
    validates :first_name_kana,  format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ }
    validates :last_name_kana,  format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ }
+  end
    validates :birth_date
    
   end
