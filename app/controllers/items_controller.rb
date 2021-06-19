580559ad911e8601d263f0739187432072b7f963
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-19 17:47:31 +0900

    実装条件のログインログアウトに関係する記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -28,7 +31,7 @@ class ItemsController < ApplicationController
    def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-       redirect_to  items_path
+       redirect_to  item_path
     else
       render :edit
     end
@@ -40,4 +43,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
+  
+  def move_to_index
+      @item = Item.find(params[:id])
+       unless current_user.id == @item.user.id
+       redirect_to action: :index
+       end
+  end
+
+
 end
