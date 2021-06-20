196e37a2cea14ad7daccc09a0d8268bf4e167a44
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-20 16:11:32 +0900

    バリデーションの処理を書く

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,3 +1,12 @@
 class PurchaseAddress
   include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :purchase_id
+  with_options presence: true do
+  validates :postal_code
+  validates :prefecture_id
+  validates :city
+  validates :addresses
+  validates :phone_number
+  end
+
 end
