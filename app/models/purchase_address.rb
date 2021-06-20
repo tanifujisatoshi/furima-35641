fa298d2c1abe15c7ca2c983c3c206493255281e6
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-20 16:32:29 +0900

    データをテーブルに保存する処理を書く

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,12 +1,21 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :purchase_id
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :user_id
   with_options presence: true do
   validates :postal_code
   validates :prefecture_id
   validates :city
   validates :addresses
-  validates :phone_number
+  validates :phone_number 
+  validates :item_id
+  validates :user_id
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(purchase_id: purchase.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, addresses: addresses, building: building, phone_number: phone_number )
+
   end
 
 end
