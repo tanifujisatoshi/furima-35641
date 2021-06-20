d6f73dc44691c8a01d4739582de7bc4e9e61b751
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-20 15:58:20 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+
+  validates :postal_code, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 }, presence: true
+  validates :city, presence: true
+  validates :addresses, presence: true
+  validates :phone_number, presence: true
+
+
+  belongs_to :purchase
 end
