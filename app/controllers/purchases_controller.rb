a62c2a427df374b3a73222e473d27e4477fd7707
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-21 20:13:58 +0900

    フォームの情報を保存できるようにする

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,5 +3,24 @@ class PurchasesController < ApplicationController
    @purchase_address = PurchaseAddress.new
   end
 
+  def create
+   
+    @purchase_address = PurchaseAddress.new(purchase_params)
+
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
+  end
+
+
+
+
   
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -13,8 +13,7 @@ class PurchaseAddress
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Address.create(purchase_id: purchase.id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, addresses: addresses, building: building, phone_number: phone_number )
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number,  purchase_id: purchase.id)
 
   end
 
