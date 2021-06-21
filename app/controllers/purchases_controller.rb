5132df0a13ffca5436d1ac339145d676ad263903
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-21 20:49:40 +0900

    エラーが表示されるようにする

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,6 +10,8 @@ class PurchasesController < ApplicationController
     if @purchase_address.valid?
       @purchase_address.save
       redirect_to root_path
+    else
+      render :index
     end
   end
 
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,11 +2,12 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :user_id
   with_options presence: true do
-  validates :postal_code
-  validates :prefecture_id
+  validates :postal_code, format: { with: /\A^\d{3}-\d{4}$\z/ }
+  #format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :prefecture_id, numericality: { other_than: 1 }
   validates :city
   validates :addresses
-  validates :phone_number 
+  validates :phone_number, format: { with: /\A0\d{9,10}\z/ }
   validates :item_id
   validates :user_id
   end
