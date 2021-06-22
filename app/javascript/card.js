755d056de76b81c8bfaef5b199d775e7188be44a
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-22 15:55:02 +0900

    トークンをサーバーサイドへ送る

---
+++ app/javascript/card.js
@@ -0,0 +1,25 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_ad3c03baa532734e7aebec17");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+    const card = {
+      number: formData.get("purchase_address[number]"),
+      cvc: formData.get("purchase_address[cvc]"),
+      exp_month: formData.get("purchase_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
