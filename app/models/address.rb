7315dffabd5bbd162b39cda74a092c53935549ba
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-22 11:49:36 +0900

    テストコードを実装する

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,10 @@
 class Address < ApplicationRecord
 
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: { with: /\A^\d{3}-\d{4}$\z/ }
   validates :prefecture_id, numericality: { other_than: 1 }, presence: true
   validates :city, presence: true
   validates :addresses, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, format: { with: /\A0\d{9,10}\z/ }
 
 
   belongs_to :purchase
--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_code              { '024-0333' }
+    prefecture_id                   { '3' }
+    city                { '北上市和賀町長沼' }
+    addresses                { '3地割14番地5' }
+    building                     { '建物名' }
+    phone_number           { '07020378633' }
+    association :purchase
   end
 end
